82a498a9ab45da85d721666ce555fa7d337b69a6
garnda <garnda.da@gmail.com>
2023-03-24 00:01:02 +0700

    Completed module1

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,16 @@
 package module1
 
 // Budget stores budget information
+type Budget struct {
+	Year     int
+	Income   float64
+	Expenses float64
+	Max      float32
+	Items    []Item
+}
 
 // Item stores item information
+type Item struct {
+	Description string
+	Price       float32
+}
